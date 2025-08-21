07c0ff5f247a163548a60b98af68ba42e79e0f62
MLwithvaibhav <thevaibhavsharma17@gmail.com>
2025-08-21 18:18:46 +0530

    Added navbar

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
 # Hello world
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return render_template('index.html')
+    # return "<p>Hello, World!</p>"
+@app.route("/products")
+def products():
+    return "<p>This is products page!</p>"
+
+if __name__ == "__main__":
+    app.run(debug=True)
