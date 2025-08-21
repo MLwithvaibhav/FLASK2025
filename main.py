574cf34ce5b3c09a468ac556e3d045db135ee7c7
MLwithvaibhav <thevaibhavsharma17@gmail.com>
2025-08-21 23:22:05 +0530

    Added static and templates

--- main.py
+++ main.py
@@ -1,7 +1,25 @@
 # Hello world
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///todo.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db = SQLAlchemy(app)
+
+
+class Todo(db.Model):
+    sno = db.Column(db.Integer, primary_key=True)
+    Title = db.Column(db.String(200), nullable=False)
+    desc = db.Column(db.String(500), nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return f"{self.sno} - {self.Title}"
+
+
+
 
 @app.route("/")
 def hello_world():
