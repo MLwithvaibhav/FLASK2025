4185a751c8b534d460e1a3d72379574681487cfa
MLwithvaibhav <thevaibhavsharma17@gmail.com>
2025-08-30 00:30:01 +0530

    Added argument parser

--- TWT FLASK RESTAPI/main3.py
+++ TWT FLASK RESTAPI/main3.py
@@ -1,8 +1,13 @@
 from flask import Flask, request
-from flask_restful import Api, Resource
+from flask_restful import Api, Resource, reqparse
 
 app = Flask(__name__)
 api = Api(app)
+
+videos_put_args = reqparse.RequestParser()
+videos_put_args.add_argument("name", type=str, help="Name of the video is required", required=True)
+videos_put_args.add_argument("likes", type=int, help="likes of the video is required", required=True)
+videos_put_args.add_argument("views", type=int, help="views of the video is required", required=True)
 
 videos = {}
 
@@ -11,8 +16,8 @@ class Video(Resource):
         return videos[video_id]
     
     def put(self, video_id):
-        print(request.form['likes'])
-        return {}
+        args = videos_put_args.parse_args()
+        return {video_id: args}
     
 api.add_resource(Video, '/video/<int:video_id>')
 
