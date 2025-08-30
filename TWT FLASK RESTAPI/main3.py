3b0cb01373268af1d1d167351f05f29808c1cffb
MLwithvaibhav <thevaibhavsharma17@gmail.com>
2025-08-30 17:32:24 +0530

    Added databases

--- TWT FLASK RESTAPI/main3.py
+++ TWT FLASK RESTAPI/main3.py
@@ -1,23 +1,55 @@
 from flask import Flask, request
-from flask_restful import Api, Resource, reqparse
+from flask_restful import Api, Resource, reqparse, abort , fields , marshal_with
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 api = Api(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+db = SQLAlchemy(app)
+
+
+class VideoModel(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    views = db.Column(db.Integer, nullable=False)
+    likes = db.Column(db.Integer, nullable=False)
+
+    def __repr__(self):
+        return f"Video(name = {self.name}, views = {self.views}, likes = {self.likes})"
+    
+
+
 
 videos_put_args = reqparse.RequestParser()
-videos_put_args.add_argument("name", type=str, help="Name of the video is required", required=True)
-videos_put_args.add_argument("likes", type=int, help="likes of the video is required", required=True)
-videos_put_args.add_argument("views", type=int, help="views of the video is required", required=True)
+videos_put_args.add_argument("name", type=str,  help="Name of the video is required", required = True, location="json")
+videos_put_args.add_argument("likes", type=int,  help="Likes of the video", required = True, location="json")
+videos_put_args.add_argument("views", type=int,  help="Views of the video" , required = True, location="json")
 
-videos = {}
+
+resource_fields = {
+    'id': fields.Integer,
+    'name': fields.String,
+    'views': fields.Integer,
+    'likes': fields.Integer
+}
+
+
 
 class Video(Resource):
+    @marshal_with(resource_fields)
     def get(self, video_id):
-        return videos[video_id]
-    
+        result = VideoModel.query.get(id=video_id)
+        return result
+
+    @marshal_with(resource_fields)
     def put(self, video_id):
         args = videos_put_args.parse_args()
-        return {video_id: args}
+        video = VideoModel(id=video_id, name=args['name'], views=args['views'], likes=args['likes'])
+        db.session.add(video)
+        db.session.commit()
+        return video, 201
+    
+    
     
 api.add_resource(Video, '/video/<int:video_id>')
 
--- TWT FLASK RESTAPI/test.py
+++ TWT FLASK RESTAPI/test.py
@@ -2,5 +2,17 @@ import requests
 
 BASE = "http://127.0.0.1:5000"
 
-response = requests.put(BASE + "/video/1" , {"likes": 10})
+data = [{"likes": 10, "views": 1000000, "name": "Vaibhav"},
+        {"likes": 20, "views": 2000000, "name": "Jahnvi"},
+        {"likes": 30, "views": 3000000, "name": "Miesha"},]
+
+for i in range(len(data)):
+    response = requests.put(BASE + "/video/1" + str(i)  ,json = data[i])
+    print(response.json())
+
+# input()
+# response = requests.delete(BASE + "/video/0" )
+# print(response)  
+input()
+response = requests.get(BASE + "/video/2" )
 print(response.json())
