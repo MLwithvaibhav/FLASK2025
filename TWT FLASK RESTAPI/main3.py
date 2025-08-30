510d3affc8103f0aa06dd5ea879a68a8dbffa311
MLwithvaibhav <thevaibhavsharma17@gmail.com>
2025-08-30 23:26:38 +0530

    completed the flask rest api

--- TWT FLASK RESTAPI/main3.py
+++ TWT FLASK RESTAPI/main3.py
@@ -26,6 +26,12 @@ videos_put_args.add_argument("likes", type=int,  help="Likes of the video", requ
 videos_put_args.add_argument("views", type=int,  help="Views of the video" , required = True, location="json")
 
 
+video_update_args = reqparse.RequestParser()
+video_update_args.add_argument("name", type=str,  help="Name of the video is required", location="json")
+video_update_args.add_argument("likes", type=int,  help="Likes of the video", location="json")
+video_update_args.add_argument("views", type=int,  help="Views of the video" , location="json")
+
+
 resource_fields = {
     'id': fields.Integer,
     'name': fields.String,
@@ -38,18 +44,45 @@ resource_fields = {
 class Video(Resource):
     @marshal_with(resource_fields)
     def get(self, video_id):
-        result = VideoModel.query.get(id=video_id)
+        result = VideoModel.query.get.filter_by(id=video_id).first()
+        if not result:
+            abort(404, message="Could not find video with that id...")
         return result
 
     @marshal_with(resource_fields)
     def put(self, video_id):
         args = videos_put_args.parse_args()
+        result = VideoModel.query.filter_by(id=video_id).first()
+        if result:
+            abort(409, message="Video id taken...")
         video = VideoModel(id=video_id, name=args['name'], views=args['views'], likes=args['likes'])
         db.session.add(video)
         db.session.commit()
         return video, 201
     
-    
+    @marshal_with(resource_fields)
+    def patch(self, video_id):
+        args = video_update_args.parse_args()
+        result = VideoModel.query.filter_by(id=video_id).first()
+        if not result:
+           abort(404, message="Video doesn't exist, cannot update...") 
+
+        if args['name']:
+            result.name = args['name']
+        if args['views']:
+            result.views = args['views']
+        if args['likes']:
+            result.likes = args['likes']
+        
+        db.session.commit()
+
+        return result
+
+
+    # def delete(self, video_id):
+    #     abort_if_video_doesnt_exist(video_id)
+    #     del videos[video_id]
+    #     return '', 204
     
 api.add_resource(Video, '/video/<int:video_id>')
 
--- TWT FLASK RESTAPI/test.py
+++ TWT FLASK RESTAPI/test.py
@@ -2,17 +2,17 @@ import requests
 
 BASE = "http://127.0.0.1:5000"
 
-data = [{"likes": 10, "views": 1000000, "name": "Vaibhav"},
-        {"likes": 20, "views": 2000000, "name": "Jahnvi"},
-        {"likes": 30, "views": 3000000, "name": "Miesha"},]
+# data = [{"likes": 10, "views": 1000000, "name": "Vaibhav"},
+#         {"likes": 20, "views": 2000000, "name": "Jahnvi"},
+#         {"likes": 30, "views": 3000000, "name": "Miesha"},]
 
-for i in range(len(data)):
-    response = requests.put(BASE + "/video/1" + str(i)  ,json = data[i])
-    print(response.json())
+# for i in range(len(data)):
+#     response = requests.put(BASE + "/video/" + str(i)  ,json = data[i])
+#     print(response.json())
 
 # input()
 # response = requests.delete(BASE + "/video/0" )
 # print(response)  
-input()
-response = requests.get(BASE + "/video/2" )
+# input()
+response = requests.patch(BASE + "/video/2" , json={"views": 5000000 , "likes": 50}) 
 print(response.json())
